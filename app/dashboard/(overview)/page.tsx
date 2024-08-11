3fef08558eab0837e0f330865baa9961cb7b5af3
NtsDK <ghoul71@yandex.ru>
2024-08-12 04:45:25 +0500

    Отключил рендер на overview странице

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -8,48 +8,24 @@ import {
   fetchCardData,
 } from "@/app/lib/data";
 import { Suspense } from "react";
-import {
-  LatestInvoicesSkeleton,
-  RevenueChartSkeleton,
-  CardsSkeleton,
-} from "@/app/ui/skeletons";
+import { LatestInvoicesSkeleton, RevenueChartSkeleton, CardsSkeleton } from "@/app/ui/skeletons";
 
 export default async function Page() {
-  // const revenue = await fetchRevenue();
-  // const latestInvoices = await fetchLatestInvoices();
-  // const {
-  //   totalPaidInvoices,
-  //   numberOfCustomers,
-  //   numberOfInvoices,
-  //   totalPendingInvoices,
-  // } = await fetchCardData();
   return (
     <main>
-      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
-        Dashboard
-      </h1>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>Dashboard</h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Suspense fallback={<CardsSkeleton />}>
+        {/* <Suspense fallback={<CardsSkeleton />}>
           <CardWrapper />
-        </Suspense>
-        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
-        <Card title="Pending" value={totalPendingInvoices} type="pending" />
-        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-        <Card
-          title="Total Customers"
-          value={numberOfCustomers}
-          type="customers"
-        /> */}
+        </Suspense> */}
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        {/* <RevenueChart revenue={revenue} /> */}
-        <Suspense fallback={<RevenueChartSkeleton />}>
+        {/* <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
         <Suspense fallback={<LatestInvoicesSkeleton />}>
           <LatestInvoices />
-        </Suspense>
-        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+        </Suspense> */}
       </div>
     </main>
   );
